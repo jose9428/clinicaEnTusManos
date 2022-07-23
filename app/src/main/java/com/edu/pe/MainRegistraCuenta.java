d74726272f2025fabbf2f473986fb1ef30749192
Jose Luis Castro <76067475+jose9428@users.noreply.github.com>
2022-07-23 08:28:56 -0500

    Integracion Pantallas

--- app/src/main/java/com/edu/pe/MainRegistraCuenta.java
+++ app/src/main/java/com/edu/pe/MainRegistraCuenta.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainRegistraCuenta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registra_cuenta);
     }
 
     public void RegistrarCuenta(View v){
         Intent intent = new Intent(this , MainRegistraCuenta.class);
+        startActivity(intent);
+    }
+
+    public void IniciarSesion(View v){
+        Intent intent = new Intent(this , MainActivity.class);
         startActivity(intent);
     }
 }
